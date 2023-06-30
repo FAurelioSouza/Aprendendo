cc606ace19e91b5823e392a1fcb9f901b54f51e3
Felipe Aurélio <faurelio_s@hotmail.com>
2023-06-30 16:58:36 -0300

    metodos novos

--- main.py
+++ main.py
@@ -3,32 +3,55 @@ import time
 import keyboard
 import random
 
-imagem = './highalchy.png'
+#imagem = './alchy.png'
+imagem = './skillAlchy.png'
 axe = './axe.png'
 dagger = './runedagger.png'
 scimitar = './steelscimitar.png'
+cannon = './reload.png'
+safespot = './safespot.png'
+
+alch1 = './alch1.png'
+alch2 = './alch2.png'
+alch3 = './alch3.png'
+
+arrow = './arrowsM.png'
+pena = './pena.png'
+
+logNote = './log.png'
+log = './logg.png'
+bank = './bank.png'
+knif = './knif.png'
 
 def highAlchemy():
-    while 1:
-        if pyautogui.locateOnScreen(imagem, confidence=0.5):
+    pause = 1
+    while pause:
+        if pyautogui.locateOnScreen(imagem, confidence=0.6):
             print("Encontrou a magia")
-            valor = pyautogui.locateOnScreen(imagem, confidence=0.5)
+            valor = pyautogui.locateOnScreen(imagem, confidence=0.6)
             valorCenter = pyautogui.center((valor[0], valor[1],valor[2], valor[3]))
             print(valor)
             print(valorCenter)
             pyautogui.moveTo(valorCenter[0], valorCenter[1], 0.3, logScreenshot=False)
-            time.sleep(0.7)
             pyautogui.click()
-            time.sleep(0.7)
-            valor = pyautogui.locateOnScreen(axe, confidence=0.5)
-            valorCenter = pyautogui.center((valor[0], valor[1],valor[2], valor[3]))
-            pyautogui.moveTo(valorCenter[0], valorCenter[1], 0.3, logScreenshot=False)
-            time.sleep(0.7)
-            pyautogui.click()
-            time.sleep(0.7)
+
+            if pyautogui.locateOnScreen(alch1, confidence=0.5):
+                valor = pyautogui.locateOnScreen(alch1, confidence=0.5)
+                valorCenter = pyautogui.center((valor[0], valor[1],valor[2], valor[3]))
+                pyautogui.moveTo(valorCenter[0], valorCenter[1], 0.3, logScreenshot=False)
+                pyautogui.click()
+                time.sleep(0.7)
+            elif pyautogui.locateOnScreen(alch2, confidence=0.5):
+                valor = pyautogui.locateOnScreen(alch2, confidence=0.5)
+                valorCenter = pyautogui.center((valor[0], valor[1],valor[2], valor[3]))
+                pyautogui.moveTo(valorCenter[0], valorCenter[1], 0.3, logScreenshot=False)
+                pyautogui.click()
+                time.sleep(0.7)
+            else:
+                pause = 0
         else:
             print("Não encontrou a magia")
-            pyautogui.press('f4')
+            pyautogui.press('f6')
             time.sleep(0.7)
 
 def afkSplash():
@@ -53,4 +76,96 @@ def returnKey(valor):
         case 4:
             return 'right'
 
-afkSplash()
+def cannonAFK():
+    while 1:
+        print("loop")
+        if pyautogui.locateOnScreen(cannon, confidence=0.5):
+            print("Reload Cannon")
+            valor = pyautogui.locateOnScreen(cannon, confidence=0.5)
+            valorCenter = pyautogui.center((valor[0], valor[1],valor[2], valor[3]))
+            print(valor)
+            print(valorCenter)
+            pyautogui.moveTo(valorCenter[0], valorCenter[1], 0.4, logScreenshot=False)
+            time.sleep(0.7)
+            pyautogui.click()
+            time.sleep(2)
+
+            valor = pyautogui.locateOnScreen(safespot, confidence=0.9)
+            valorCenter = pyautogui.center((valor[0], valor[1],valor[2], valor[3]))
+            pyautogui.moveTo(valorCenter[0], valorCenter[1], 0.3, logScreenshot=False)
+            time.sleep(0.7)
+            pyautogui.click()
+            time.sleep(0.7)
+
+def fletchingFeather():
+    pause = 1
+    while pause:
+        if pyautogui.locateOnScreen(arrow, confidence=0.7):
+            print("Encontrou a magia")
+            valor = pyautogui.locateOnScreen(arrow, confidence=0.7)
+            valorCenter = pyautogui.center((valor[0], valor[1],valor[2], valor[3]))
+            print(valor)
+            print(valorCenter)
+            pyautogui.moveTo(valorCenter[0], valorCenter[1], 0.3, logScreenshot=False)
+            pyautogui.click()
+
+            if pyautogui.locateOnScreen(pena, confidence=0.8):
+                valor = pyautogui.locateOnScreen(pena, confidence=0.8)
+                valorCenter = pyautogui.center((valor[0], valor[1],valor[2], valor[3]))
+                pyautogui.moveTo(valorCenter[0], valorCenter[1], 0.3, logScreenshot=False)
+                pyautogui.click()
+                time.sleep(0.7)
+                pyautogui.press('space')
+                time.sleep(10.5)
+            else:
+                pause = 0
+        else:
+            print("Não encontrou os items")
+            pause = 0
+            time.sleep(0.7)
+
+def fletchingLOG():
+    pause = 1
+    while pause:
+        if pyautogui.locateOnScreen(logNote, confidence=0.8):
+            print("Encontrou a magia")
+            valor = pyautogui.locateOnScreen(logNote, confidence=0.8)
+            valorCenter = pyautogui.center((valor[0], valor[1],valor[2], valor[3]))
+            print(valor)
+            print(valorCenter)
+            pyautogui.moveTo(valorCenter[0], valorCenter[1], 0.3, logScreenshot=False)
+            pyautogui.click()
+
+            if pyautogui.locateOnScreen(bank, confidence=0.5):
+                valor = pyautogui.locateOnScreen(bank, confidence=0.5)
+                valorCenter = pyautogui.center((valor[0], valor[1],valor[2], valor[3]))
+                pyautogui.moveTo(valorCenter[0], valorCenter[1], 0.3, logScreenshot=False)
+                pyautogui.click()
+                time.sleep(0.9)
+                pyautogui.press('1')
+                time.sleep(0.7)
+                if pyautogui.locateOnScreen(knif, confidence=0.6):
+                    valor = pyautogui.locateOnScreen(knif, confidence=0.6)
+                    valorCenter = pyautogui.center((valor[0], valor[1],valor[2], valor[3]))
+                    pyautogui.moveTo(valorCenter[0], valorCenter[1], 0.3, logScreenshot=False)
+                    pyautogui.click()
+                    time.sleep(0.7)
+                    if pyautogui.locateOnScreen(log, confidence=0.8):
+                        valor = pyautogui.locateOnScreen(log, confidence=0.8)
+                        valorCenter = pyautogui.center((valor[0], valor[1],valor[2], valor[3]))
+                        print(valor)
+                        print(valorCenter)
+                        pyautogui.moveTo(valorCenter[0], valorCenter[1], 0.3, logScreenshot=False)
+                        pyautogui.click()
+                        time.sleep(0.9)
+                        pyautogui.press('space')
+                        time.sleep(30)
+
+            else:
+                pause = 0
+        else:
+            print("Não encontrou os items")
+            pause = 0
+            time.sleep(0.7)
+
+fletchingLOG()
